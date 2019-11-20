780d9a670003d612f7361309a264572ae73bc2c6
Monsur Khan <monsur.khan@gmail.com>
2019-11-20 21:58:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,19 @@
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
-  def initialize(brand)
-    @brand = brand
+  GENRES = []
+
+  def initialize(title)
+    @title = title
   end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
